c96c85f665d26363731bb7da2fc6714c4793ac9a
liuxuegang <liuxuegang@meitunmama.com>
2023-05-10 17:21:34 +0800

    初始化

---
+++ src/main/scala/com/sca/common/ScalaApp.scala
@@ -0,0 +1,13 @@
+package com.sca.common
+
+object ScalaApp  {
+
+
+    def main(args: Array[String]): Unit = {
+      val x = "scala"
+      val result = if (x.length == 5) "true" else "false"
+      print(result)
+    }
+
+
+}
